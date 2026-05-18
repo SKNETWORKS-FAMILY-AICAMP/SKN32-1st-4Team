4b46458da16552c9e5f7bb3bd0f08029d218b05f
4nchez <58410037+4nchez@users.noreply.github.com>
2026-05-18 23:25:10 +0900

    jjh430 작업 내역 반영 - 현 프로젝트에 맞게 수정
    Co-authored-by: jjh430 <jjh2002x@gmail.com>

--- service/dashboard_service.py
+++ service/dashboard_service.py
@@ -28,3 +28,24 @@ class DashboardService():
     
     def get_district(self):
         return self.__db_service.select_district(DistrictSearchDTO(order_clauses=[("region_code", None), ("code", None)]))
+
+
+    # FIXME: 필히 수정해야 함
+    def get_vehicle_data_from_db(self,):
+        import pandas as pd
+        engine = self.__db_service.engine
+        
+        # 여러 테이블을 JOIN하여 필요한 정보를 한 번에 가져옵니다.
+        query = """
+        SELECT 
+            v.registration_date AS 등록월,
+            c.name AS 차량종류,
+            r.name AS 시도,
+            d.name AS 시군구,
+            v.vehicles AS 등록차량수
+        FROM vehicle_registration_status v
+        LEFT JOIN vehicle_type c ON v.type = c.code
+        LEFT JOIN region r ON v.region = r.code
+        LEFT JOIN district d ON v.district = d.code
+        """
+        return pd.read_sql(query, engine)
